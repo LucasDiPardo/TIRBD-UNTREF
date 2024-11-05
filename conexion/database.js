f020472155b09ebc6dcaf7dcd2233b3d76c1542a
lucasdipardo <lucasdipardo1994@gmail.com>
2024-11-05 13:17:30 -0300

    cambios

--- conexion/database.js
+++ conexion/database.js
@@ -1,21 +1,6 @@
 const { Sequelize } = require("sequelize");
-// const dotenv = require("dotenv");
 
-// // let envFile;
-// // switch (process.env.NODE_ENV) {
-// //   case "production":
-// //     envFile = ".env.production";
-// //     break;
-// //   case "local_railway":
-// //     envFile = ".env.local_railway";
-// //     break;
-// //   default:
-// //     envFile = ".env.local";
-// // }
-
-// dotenv.config({ path: envFile });
-
-const { DBUSER, PASSWORD, HOST, DATABASE, PORT, DIALECT } = process.env;
+const { DBUSER, PASSWORD, HOST, DATABASE, PORT } = process.env;
 
 const sequelize = new Sequelize(DATABASE, DBUSER, PASSWORD, {
   host: HOST,
